dde0b14360952d8ad7436458544cad63035ffe74
Fabio Maximo <fabiomaximoc@gmail.com>
2024-07-18 10:03:37 -0300

    Finish masks inputs

--- js/script.js
+++ js/script.js
@@ -45,3 +45,115 @@ function attachTopScroller (elementId){
       return false;
   });
 };
+
+
+
+
+$(".send--payment").click(function(){
+  $(this).addClass("act");
+  $("form.needs-validation>div").removeClass("act");
+  $(".register__section--two").addClass("act");
+  $(".select--checkout-pagamento").addClass("act");
+});
+
+$(".send-agradecimento").click(function(){
+  $(this).addClass("act");
+  $("form.needs-validation>div").removeClass("act");
+  $(".register__section--three").addClass("act");
+  $(".select--checkout-agradecimento").addClass("act");
+});
+
+
+
+
+
+$(document).ready(function() {
+  var elemcep = $("#cep");
+  if (elemcep) {
+      elemcep.keyup(function() {
+
+          if (elemcep.val().length == 8){
+            elemcep = elemcep.val();
+
+            $.ajax({
+              url: "https://viacep.com.br/ws/"+elemcep+"/json/",
+              type: "GET",
+              dataType: "json",
+              success: function (data) {
+    
+                  if(data.erro !== undefined){
+                      alert("CEP inválido ou não encontrado");
+                  }else{
+                      $("#endereco").val(data.logradouro);
+                      $("#bairro").val(data.bairro);
+                      $("#cidade").val(data.localidade);
+                      $("#estado").val(data.uf);
+                      $(".adress--infos").removeClass("none");
+                  }
+              },
+              error: function(data){
+                  alert("Algum erro ocorreu, consulte o log.");
+              },
+              complete: function(){
+                  //loading.hide();
+              }
+            });
+
+          }
+      });
+  }
+  
+
+
+
+$("#cpfcnpj").keydown(function(){
+  try {
+      $("#cpfcnpj").unmask();
+  } catch (e) {}
+
+  var tamanho = $("#cpfcnpj").val().length;
+
+  if(tamanho < 11){
+      $("#cpfcnpj").mask("999.999.999-99");
+  } else {
+      $("#cpfcnpj").mask("99.999.999/9999-99");
+  }
+
+  // ajustando foco
+  var elem = this;
+  setTimeout(function(){
+      // mudo a posição do seletor
+      elem.selectionStart = elem.selectionEnd = 10000;
+  }, 0);
+  // reaplico o valor para mudar o foco
+  var currentValue = $(this).val();
+  $(this).val('');
+  $(this).val(currentValue);
+});
+
+
+
+
+var minutos = 60;
+var segundos = 0;
+var timer = setInterval(function() {
+  if (segundos > 0) {
+    segundos--;
+  } else {
+    if (minutos > 0) {
+      minutos--;
+      segundos = 59;
+    } else {
+      clearInterval(timer);
+      $("#contador").html("Tempo esgotado!");
+      return;
+    }
+  }
+  $("#contador").html(minutos + ":" + (segundos < 10 ? "0" : "") + segundos);
+}, 1000);
+
+$("#telefone").mask("(00) 0000-00009");
+
+
+
+});
